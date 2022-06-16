a0d4109b8168a08013777b94f6ce4f78b00159fd
Rinat Saitov <rsaitov@mail.ru>
2022-06-16 19:02:49 +0300

    Prepare project for EF migration

---
+++ .net/lesja/net6-minimal-api/SixMinApi/Program.cs
@@ -0,0 +1,29 @@
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using SixMinApi.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var sqlConnectionBuilder = new SqlConnectionStringBuilder();
+sqlConnectionBuilder.ConnectionString = builder.Configuration.GetConnectionString("LocalDbConnection");
+sqlConnectionBuilder.UserID = builder.Configuration["UserId"];
+sqlConnectionBuilder.Password = builder.Configuration["Password"];
+
+builder.Services.AddDbContext<AppDbContext>(opt =>
+    opt.UseSqlServer(sqlConnectionBuilder.ConnectionString));
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
+
