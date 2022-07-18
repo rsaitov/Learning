6754b7033c5d8f944ab7e4fd8cd9cca97ca3bcb5
Rinat Saitov <rsaitov@mail.ru>
2022-07-18 22:01:06 +0300

    DiApi project finished

--- .net/lesja/net-developer-toolkit/DI/DiApi/Data/NoSqlDataRepo.cs
+++ .net/lesja/net-developer-toolkit/DI/DiApi/Data/NoSqlDataRepo.cs
@@ -1,14 +1,40 @@
+using DiApi.DataServices;
+
 namespace DiApi.Data
 {
     public class NoSqlDataRepo : IDataRepo
     {
+        //private readonly IDataService _dataService;
+        //public NoSqlDataRepo(IDataService dataService)
+        // {
+        //     _dataService = dataService;
+        // }
+
+        private readonly IServiceScopeFactory _scopeFactory;
+        public NoSqlDataRepo(IServiceScopeFactory scopeFactory)
+        {
+            _scopeFactory = scopeFactory;
+        }
+
         public string GetData()
         {
             Console.ForegroundColor = ConsoleColor.Red;
-            System.Console.WriteLine("--> Getting data from No SQL Database");
-            Console.ResetColor();
+            Console.WriteLine("--> Getting data from No SQL Database");
 
-            return("No SQL Data from DB");
+            using (var scope = _scopeFactory.CreateScope())
+            {
+                var dataService = scope.ServiceProvider.GetRequiredService<IDataService>();
+
+                dataService.GetProductData("https://something.com/api");
+
+                Console.ResetColor();
+                return ("No SQL Data from DB");
+            }
+
+            // ctor-di instructions
+            //_dataService.GetProductData("https://something.com/api");
+            // Console.ResetColor();
+            // return ("No SQL Data from DB");
         }
 
         public string ReturnData() => GetData();
---
+++ .net/lesja/net-developer-toolkit/DI/DiApi/Utility/IOperation.cs
@@ -0,0 +1,11 @@
+namespace DiApi.Utility
+{
+    public interface IOperation
+    {
+        public string OperationId { get; }
+    }
+
+    public interface IOperationTransient : IOperation {}
+    public interface IOperationScoped : IOperation {}
+    public interface IOperationSingleton : IOperation {}
+}
