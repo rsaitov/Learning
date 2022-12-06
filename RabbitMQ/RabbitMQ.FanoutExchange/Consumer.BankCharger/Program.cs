1fc02e8a992868e1a7db57b051252903f085b0cc
Rinat Saitov <rsaitov@mail.ru>
2022-12-06 11:39:39 +0300

    Exhange types projects

---
+++ RabbitMQ/RabbitMQ.FanoutExchange/Consumer.BankCharger/Program.cs
@@ -0,0 +1,51 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var exchangeName = "notifier";
+var totalHold = 0;
+var factory = new ConnectionFactory { HostName = "localhost" };
+
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    channel.ExchangeDeclare(
+        exchange: exchangeName,
+        type: ExchangeType.Fanout);
+
+    var queueName = channel.QueueDeclare().QueueName;
+
+    channel.QueueBind(
+        queue: queueName, 
+        exchange: exchangeName,
+        routingKey: string.Empty);
+
+    var consumer = new EventingBasicConsumer(channel);
+
+    consumer.Received += (sender, e) =>
+    {
+        var body = e.Body;
+        var message = Encoding.UTF8.GetString(body.ToArray());
+
+        var payment = GetPayment(message);
+        totalHold += payment;
+        Console.WriteLine($"Payment received: {payment}");
+        Console.WriteLine($"Total hold: {totalHold}");
+    };
+
+    channel.BasicConsume(
+        queue: queueName,
+        autoAck: true,
+        consumer: consumer);
+
+    Console.WriteLine($"Subscribed to the queue '{queueName}'");
+    Console.WriteLine($"Listening...");
+
+    Console.ReadLine();
+}
+
+int GetPayment(string message)
+{
+    var messageWords = message.Split(' ');
+    return int.Parse(messageWords[^1]);
+}
---
+++ RabbitMQ/RabbitMQ.TopicExchange/Producer/Program.cs
@@ -0,0 +1,51 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var counter = 0;
+
+do
+{
+    int timeToSleep = random.Next(1000, 2000);
+    Thread.Sleep(timeToSleep);
+
+    var factory = new ConnectionFactory { HostName = "localhost" };
+
+    using (var connection = factory.CreateConnection())
+    using (var channel = connection.CreateModel())
+    {
+        channel.ExchangeDeclare(
+            exchange: "topic_logs", 
+            type: ExchangeType.Topic);
+
+        var routingKey = counter % 4 == 0
+            ? "Tesla.red.fast.ecological"
+            : counter % 5 == 0
+                ? "Mercedes.exclusive.expensive.ecological"
+                : GenerateRoutingKey();
+
+        var message = $"Message type [{routingKey}] from publisher [N:{counter}]";
+
+        var body = Encoding.UTF8.GetBytes(message);
+
+        channel.BasicPublish(
+            exchange: "topic_logs",
+            routingKey: routingKey,
+            basicProperties: null,
+            body: body
+         );
+
+        Console.WriteLine($"Message type [{routingKey}] is sent into Topic Exchange N {counter++}");
+    }
+} while (true);
+
+static string GenerateRoutingKey()
+{
+    return $"{cars[random.Next(0, cars.Count)]}.{colors[random.Next(0,colors.Count)]}";
+}
+
+public partial class Program
+{
+    static List<string> cars = new List<string> { "BMW", "Audi", "Tesla", "Mercedes" };
+    static List<string> colors = new List<string> { "red", "white", "black" };
+    static Random random = new Random();
+}
