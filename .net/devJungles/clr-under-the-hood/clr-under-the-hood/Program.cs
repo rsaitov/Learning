fb8912bc8a14f5fe8fbf3925659589e980d34111
Rinat Saitov <rsaitov@mail.ru>
2022-12-28 01:25:56 +0400

    CLR under the hood added

---
+++ .net/devJungles/clr-under-the-hood/clr-under-the-hood/Program.cs
@@ -0,0 +1,17 @@
+﻿var str = "Hello, World!";
+var ptrStr = AddressOf(str);
+Console.WriteLine($"str address: {ptrStr}");
+
+var ptrInt = AddressOf(17);
+Console.WriteLine($"int address: {ptrInt}");
+
+uint[] arr = { 0xAABBCCDD, 0xAABBCCDD, 0xAABBCCDD, 0xAABBCCDD, 0xFFFFFFFF };
+var ptrArr = AddressOf(arr);
+Console.WriteLine($"arr address: {ptrArr}");
+
+// return RAM address of the object
+unsafe IntPtr AddressOf(object o)
+{
+    TypedReference mk = __makeref(o);
+    return **(IntPtr**)&mk;
+}
