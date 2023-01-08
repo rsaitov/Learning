152147bf012d156920a9e2d593879d5442bde990
Rinat Saitov <rsaitov@mail.ru>
2023-01-08 22:32:06 +0400

    CLR under the hood: struct in memory

--- .net/devJungles/clr-under-the-hood/clr-under-the-hood/01_PointerAddress.cs
+++ .net/devJungles/clr-under-the-hood/clr-under-the-hood/01_PointerAddress.cs
@@ -20,6 +20,14 @@ namespace clr_under_the_hood
             uint[] arr = { 0xAABBCCDD, 0xAABBCCDD, 0xAABBCCDD, 0xAABBCCDD, 0xFFFFFFFF };
             var ptrArr = AddressViewer.AddressOf(arr);
             Console.WriteLine($"arr address: {ptrArr}");
+            
+            var myStruct1 = new SimpleStruct2() { B1 = 0xAA, I = 0xBBBBBBBB, S = 0xEEEE, S1 = "Hello" };
+            var ptrMyStruct1 = AddressViewer.AddressOf(myStruct1);
+            Console.WriteLine($"myStruct address: {ptrMyStruct1}");
+
+            var myStruct2 = new SimpleStruct2() { B1 = 0xBB, I = 0xCCBBBBBB, S = 0xDDEE, S1 = "World" };
+            var ptrMyStruct2 = AddressViewer.AddressOf(myStruct2);
+            Console.WriteLine($"myStruct address: {ptrMyStruct2}");
 
             SimpleStruct[] arrStruct = {
                 new SimpleStruct() { B1 = 0xAA, I = 0xBBBBBBBB, S = 0xEEEE },
--- .net/devJungles/clr-under-the-hood/clr-under-the-hood/SimpleStruct.cs
+++ .net/devJungles/clr-under-the-hood/clr-under-the-hood/SimpleStruct.cs
@@ -9,4 +9,13 @@ namespace clr_under_the_hood
         public ushort S;
         public uint I;
     }
+
+    [StructLayout(LayoutKind.Sequential)]   // guarantees memory usage order in RAM
+    struct SimpleStruct2
+    {
+        public byte B1;
+        public ushort S;
+        public uint I;
+        public string S1;
+    }
 }
